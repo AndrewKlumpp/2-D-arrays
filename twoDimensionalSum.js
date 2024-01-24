5581fb98e141c875ef24ef4e842d4ce9803748df
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-24 16:32:35 -0700

    initial commit

---
+++ twoDimensionalSum.js
@@ -0,0 +1,16 @@
+// your code here
+
+
+
+let arr1 = [
+    [1, 3],
+    [-4, 7, 10],
+    [2]
+];
+console.log(twoDimensionalSum(arr1)); // 19
+
+let arr2 = [
+    [],
+    [3, 1, 2],
+];
+console.log(twoDimensionalSum(arr2)); // 6
