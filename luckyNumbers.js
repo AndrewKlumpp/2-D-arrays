cd9c4d5bd58b69153aa881c735fb2f44dc53f4dc
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 13:48:53 -0700

    finished luckyNumbers

--- luckyNumbers.js
+++ luckyNumbers.js
@@ -1,6 +1,44 @@
+function highestNum(arr) {
+  let highNum = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+
+    if (num > highNum) {
+      highNum = num;
+    }
+  }
+  return highNum;
+};
+
+function lowestNum(arr) {
+  let lowNum = arr[0];
+
+  for (let i = 0; i < arr.length; i++) {
+    let num = arr[i];
+
+    if (num < lowNum) {
+      lowNum = num;
+    }
+  }
+  return lowNum;
+};
+
 function luckyNumbers(matrix) {
-  // your code here...
-}
+  for (let i = 0; i < matrix.length; i++) {
+    let row = matrix[i];
+    let lowNum = lowestNum(row);
+    let lowNumColumn = row.indexOf(lowNum);
+    let colArr = [];
+
+    for (let j = 0; j < matrix.length; j++) {
+      let num = matrix[j][lowNumColumn];
+      colArr.push(num);
+    }
+    let highNum = highestNum(colArr);
+    if (lowNum === highNum) return [lowNum];
+  }
+};
 
 
 matrix = [[ 5,  9, 21],
