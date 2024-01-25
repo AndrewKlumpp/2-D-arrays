9ac862c20d7a6d22ab814f7a77a022209885b625
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 14:44:09 -0700

    finished spiralOrder

--- spiralOrder.js
+++ spiralOrder.js
@@ -1,5 +1,39 @@
 function spiralOrder(matrix) {
-  // your code here...
+  let rows = matrix.length;
+  let row = 0;
+  let cols = matrix[0].length;
+  let col = 0;
+  let pushed = 0;
+  let needPushed = rows * cols;
+  let output = [];
+
+  while (pushed < needPushed) {
+    for (let i = col; i < cols; i++) {
+      output.push(matrix[row][i]);
+      pushed++;
+    }
+    row++;
+
+    for (let i = row; i < rows; i++) {
+      output.push(matrix[i][cols - 1]);
+      pushed++;
+    }
+    cols--;
+
+    for (let i = cols - 1; i >= col; i--) {
+      output.push(matrix[rows - 1][i]);
+      pushed++;
+    }
+    rows--;
+
+    for (let i = rows - 1; i >= row; i--) {
+      output.push(matrix[i][col]);
+      pushed++;
+    }
+    col++;
+  }
+
+  return output;
 };
 
 
