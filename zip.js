ca5054e1efe5634f1488a1522395706a128933a8
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 11:19:08 -0700

    finished zip

--- zip.js
+++ zip.js
@@ -1,5 +1,11 @@
 function zip(array1, array2) {
-
+  let zipped = [];
+  for (let i = 0; i < array1.length; i++) {
+    let el1 = array1[i];
+    let el2 = array2[i];
+    zipped.push([el1, el2]);
+  }
+  return zipped;
 };
 
 console.log(zip([1, 2, 3, 4], ['eins', 'zwei', 'drei', 'vier']));
