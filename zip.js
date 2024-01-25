42afb8ff4968654852ed2c9d1e28329161e72416
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 11:09:13 -0700

    initial commit

---
+++ zip.js
@@ -0,0 +1,12 @@
+function zip(array1, array2) {
+
+};
+
+console.log(zip([1, 2, 3, 4], ['eins', 'zwei', 'drei', 'vier']));
+// [ [ 1, 'eins' ], [ 2, 'zwei' ], [ 3, 'drei' ], [ 4, 'vier' ] ]
+
+console.log(zip(['eins', 'zwei', 'drei'], [1, 2, 3]));
+// [ [ 'eins', 1 ], [ 'zwei', 2 ], [ 'drei', 3 ] ]
+
+console.log(zip(['alef', 'bet'], ['alpha', 'beta']));
+// [ [ 'alef', 'alpha' ], [ 'bet', 'beta' ] ]
