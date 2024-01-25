8e3fd81e39ab4abd3473dc4b18c573be7a4ba90e
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 11:21:19 -0700

    initial commit

---
+++ zanyZip.js
@@ -0,0 +1,12 @@
+function zanyZip(array1, array2) {
+
+};
+
+console.log(zanyZip([1, 2], ['eins', 'zwei', 'drei', 'vier']));
+// [ [ 1, 'eins' ], [ 2, 'zwei' ], [ null, 'drei' ], [ null, 'vier' ] ]
+
+console.log(zanyZip([1, 2, 3, 4], ['eins', 'zwei', 'drei']));
+// [ [ 1, 'eins' ], [ 2, 'zwei' ], [ 3, 'drei' ], [ 4, null ] ]
+
+console.log(zanyZip(['alef', 'bet'], ['alpha', 'beta']));
+// [ [ 'alef', 'alpha' ], [ 'bet', 'beta' ] ]
