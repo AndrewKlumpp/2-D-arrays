7cd8a86b5eb616b3854e5a9c0664dc1948704676
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 11:33:51 -0700

    finished zanyZip

--- zanyZip.js
+++ zanyZip.js
@@ -1,5 +1,17 @@
 function zanyZip(array1, array2) {
-
+  let zipped = [];
+  for (let i = 0; i < array1.length || i < array2.length; i++) {
+    let el1 = array1[i];
+    let el2 = array2[i];
+    if (el1 === undefined) {
+      el1 = null;
+    }
+    if (el2 === undefined) {
+      el2 = null;
+    }
+    zipped.push([el1, el2]);
+  }
+  return zipped;
 };
 
 console.log(zanyZip([1, 2], ['eins', 'zwei', 'drei', 'vier']));
