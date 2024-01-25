4008aaa63e38b8a200fd943367e29c43753e21e2
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 15:20:26 -0700

    first and second level of pyramid are working, need to add next levels with another loop

--- pyramidArray.js
+++ pyramidArray.js
@@ -1,5 +1,14 @@
 function pyramidArray(base) {
+  let pyramid = [];
+  pyramid.push(base);
 
+    let currentLevel = [];
+    for (let i = 0; i < pyramid[pyramid.length - 1].length - 1; i++) {
+      currentLevel.push(pyramid[0][i] + pyramid[0][i + 1]);
+    }
+    pyramid.unshift(currentLevel);
+
+  return pyramid;
 };
 
 
