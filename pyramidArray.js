5b5ad16731a7af80bc4476ca971958386bc2498d
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 14:48:37 -0700

    initial commit

---
+++ pyramidArray.js
@@ -0,0 +1,33 @@
+function pyramidArray(base) {
+
+};
+
+
+
+
+let p1 = pyramidArray([2, 3, 7, 5, 9]);
+console.log(p1);
+// [
+//   [ 85 ],
+//   [ 37, 48 ],
+//   [ 15, 22, 26 ],
+//   [ 5, 10, 12, 14 ],
+//   [ 2, 3, 7, 5, 9 ]
+// ]
+
+let p2 = pyramidArray([2, 2, 2, 2]);
+console.log(p2);
+// [
+//   [ 16 ],
+//   [ 8, 8 ],
+//   [ 4, 4, 4 ],
+//   [ 2, 2, 2, 2 ]
+// ]
+
+// For example, given 2, 3, 7, 5, 9 as the base, we should construct this pyramid:
+//
+//           85
+//         37  48
+//       15  22  26
+//    5   10   12   14
+//  2   3    7    5    9
