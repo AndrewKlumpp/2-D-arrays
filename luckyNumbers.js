980813647587a566627e8097fb60cccd4965121e
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-25 12:28:17 -0700

    initial commit

---
+++ luckyNumbers.js
@@ -0,0 +1,16 @@
+function luckyNumbers(matrix) {
+  // your code here...
+}
+
+
+matrix = [[ 5,  9, 21],
+          [ 9, 19,  6],
+          [12, 14, 15]]
+
+console.log(luckyNumbers(matrix)); // [12]
+
+matrix = [[ 5, 10,  8,  6],
+          [10,  2,  7,  9],
+          [21, 15, 19, 10]]
+
+console.log(luckyNumbers(matrix)); // [10]
