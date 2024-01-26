4ad7337c6e0bcff48137578b1fabd9b56c8faa98
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-26 09:14:34 -0700

    initial commit

---
+++ pascalsTriangle.js
@@ -0,0 +1,30 @@
+// For example, here are the first 5 levels of Pascal's triangle:
+//       1
+//      1 1
+//     1 2 1
+//    1 3 3 1
+//   1 4 6 4 1
+
+// Your code here
+
+
+
+console.log(pascalsTriangle(5));
+// [
+//     [1],
+//     [1, 1],
+//     [1, 2, 1],
+//     [1, 3, 3, 1],
+//     [1, 4, 6, 4, 1]
+// ]
+
+console.log(pascalsTriangle(7));
+// [
+//     [1],
+//     [1, 1],
+//     [1, 2, 1],
+//     [1, 3, 3, 1],
+//     [1, 4, 6, 4, 1],
+//     [1, 5, 10, 10, 5, 1],
+//     [1, 6, 15, 20, 15, 6, 1]
+// ]
