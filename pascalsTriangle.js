0b22ef5c2fe7c52e0c408e55fe3746c714fe1060
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-27 17:39:47 -0700

    finished pascalsTriangle

--- pascalsTriangle.js
+++ pascalsTriangle.js
@@ -1,12 +1,20 @@
-// For example, here are the first 5 levels of Pascal's triangle:
-//       1
-//      1 1
-//     1 2 1
-//    1 3 3 1
-//   1 4 6 4 1
+function pascalsTriangle(n) {
+  let triangle = [];
 
-// Your code here
+  for (let i = 0; i < n; i++) {
+    triangle[i] = [];
 
+    for (let j = 0; j <= i; j++) {
+      if (j === 0 || j === i) {
+        triangle[i][j] = 1;
+      } else {
+        triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+      }
+    }
+  }
+
+  return triangle;
+};
 
 
 console.log(pascalsTriangle(5));
